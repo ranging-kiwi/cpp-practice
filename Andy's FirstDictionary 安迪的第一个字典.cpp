d526687234b4e40f55ba1c0bebc9834b719a7a22
ranging_kiwi <3865245887@qq.com>
2026-02-15 00:35:38 +0800

    Update Andy's FirstDictionary 安迪的第一个字典.cpp

--- Andy's FirstDictionary 安迪的第一个字典.cpp
+++ Andy's FirstDictionary 安迪的第一个字典.cpp
@@ -10,13 +10,13 @@ int main(){
     string s,buf;
     while(cin>>s){
         for(int i=0;i<s.length();i++){
-            if(isalpha(s[i])) tolower(s[i]);
+            if(isalpha(s[i])) s[i]=tolower(s[i]);
             else s[i]=' ';
         }
         stringstream ss(s);
         while(ss>>buf) dictionary.insert(buf);
     }
-    for(set<string>::iterator it=dictionary.begin();it<dictionary.end();it++){
+    for(set<string>::iterator it=dictionary.begin();it!=dictionary.end();it++){
         cout<<*it<<'\n';
     }
     return 0;
