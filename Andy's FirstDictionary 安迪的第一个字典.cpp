35bc2c39540739783804006fb4ecd097a9f3211c
ranging_kiwi <3865245887@qq.com>
2026-02-15 00:18:20 +0800

    Create Andy's FirstDictionary 安迪的第一个字典.cpp

---
+++ Andy's FirstDictionary 安迪的第一个字典.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<set>
+using namespace std;
+
+set<string> dictionary;
+
+int main(){
+    string s,buf;
+    while(cin>>s){
+        for(int i=0;i<s.length();i++){
+            if(isalpha(s[i])) tolower(s[i]);
+            else s[i]=' ';
+        }
+        stringstream ss(s);
+        while(ss>>buf) dictionary.insert(buf);
+    }
+    for(set<string>::iterator it=dictionary.begin();it<dictionary.end();it++){
+        cout<<*it<<'\n';
+    }
+    return 0;
+}
