2875a8007a7445e67e573165118c48582ef1374b
ranging_kiwi <3865245887@qq.com>
2026-02-18 23:51:28 +0800

    Add Ugly Number calculation program
    Implement a program to find the 1500th ugly number using a priority queue and set.

---
+++ Ugly Number.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+#include<set>
+#include<queue>
+using namespace std;
+
+typedef long long ll;
+const int coeff[3]={2,3,5};
+
+int main(){
+    priority_queue<ll,vector<ll>,greater<ll>> pq;
+    set<ll> s;
+    pq.push(1);
+    s.insert(1);
+    for(int i=0;1;i++){
+        ll x=pq.top();
+        pq.pop();
+        if(i==1500){
+            cout<<"The 1500th ugly number is "<<x<<".\n";
+            break;
+        }
+        for(int j=0;j<3;j++){
+            ll x2=x*coeff[j];
+            if(s.count(x2)==0){
+                s.insert(x2);
+                pq.push(x2);
+            }
+        }
+    }
+    return 0;
+}
